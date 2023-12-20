fa14f1b55df52deef195a160b75827bb00ac0532
[Zaharaddeen Balarabe] <[zaharaddeenb@gmail.com]>
2023-12-20 14:08:13 +0100

    Exercise 3.8 added

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 var morgan = require('morgan')
 
+morgan.token('content', function(req, res) {
+    return JSON.stringify(req.body);
+});
+
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+
 
 let persons = [
     { 
